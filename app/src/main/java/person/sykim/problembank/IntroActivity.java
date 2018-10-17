c591a073aa18a67d370b9b22737ebd5342201299
김성용 <dnjstnddl1679@hanmail.net>
2018-10-18 00:24:01 +0900

    add FirebaseAuth

--- app/src/main/java/person/sykim/problembank/IntroActivity.java
+++ app/src/main/java/person/sykim/problembank/IntroActivity.java
@@ -3,18 +3,26 @@ package person.sykim.problembank;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseUiException;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,8 +31,10 @@ import person.sykim.problembank.data.ProblemBank;
 public class IntroActivity extends AppCompatActivity {
 
     private static final String TAG = "IntroActivity";
+    private static final int RC_SIGN_IN = 123;
 
     MyApplication application;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @BindView(R.id.loading_text)
     TextView loadingText;
@@ -38,11 +48,34 @@ public class IntroActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         application = (MyApplication) getApplication();
 
-        AsyncTask.execute(this::initFirebase);
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            AsyncTask.execute(this::initFirebase);
+        } else {
+            openFirebaseLogin();
+        }
+    }
+
+    public void openFirebaseLogin() {
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.mipmap.ic_launcher_round)
+                        .build(),
+                RC_SIGN_IN);
     }
 
     void initFirebase() {
         runOnUiThread(() -> loadingText.setText("Initialize firebase..."));
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            Log.i(TAG, "initFirebase: id: " + user.getUid());
+        }
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference reference = database.getReference();
@@ -81,6 +114,26 @@ public class IntroActivity extends AppCompatActivity {
             startActivity(main);
         }
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                AsyncTask.execute(this::initFirebase);
+            } else {
+                if (response != null && response.getError() != null) {
+                    FirebaseUiException exception = response.getError();
+                    FirebaseAnalytics.getInstance(this)
+                            .setUserProperty("FirebaseUI Login Error", exception.getMessage()+":"+exception.getErrorCode());
+                }
+                openFirebaseLogin();
+            }
+        }
     }
 
     @Override
