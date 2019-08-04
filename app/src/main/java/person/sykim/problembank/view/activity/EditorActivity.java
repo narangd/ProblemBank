22e4c9aa22cc52899078109f91a7b1df0ce8ed2b
김성용 <dnjstnddl1679@hanmail.net>
2019-08-04 16:23:14 +0900

    메뉴 추가 및 사용 아이콘 추가

--- app/src/main/java/person/sykim/problembank/view/activity/EditorActivity.java
+++ app/src/main/java/person/sykim/problembank/view/activity/EditorActivity.java
@@ -83,11 +83,14 @@ public class EditorActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_run:
+                return true;
+            case R.id.action_save:
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
